3fbdff3037659da3e9b830b3f8031e9075940401
Bernd Lachner <dev@lachner-net.de>
2019-04-14 18:46:25 +0200

    Copyright notice added

--- init.lua
+++ init.lua
@@ -1,3 +1,18 @@
+-- Copyright (C) 2019 Bernd Lachner <dev@lachner-net.de>
+--
+-- This program is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 -- Count the number of pictures.
 local function get_picture(number)
     local filename	= minetest.get_modpath("gallery").."/textures/picture_"..number..".png"
